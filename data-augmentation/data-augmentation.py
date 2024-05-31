cb87a8f93f0c8fcba5c825ffce3a49064b11846a
LorenzoStancato <lorenzo.stancato@agictech.com>
2024-05-31 13:05:09 +0200

    feat: add data-augmentation

---
+++ data-augmentation/data-augmentation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("./data/merged.csv")
+
+num_samples = 1000
+
+
+def generate_numerical_data(df, columns, num_sample):
+    new_data = {}
+    for col in columns:
+        mean = df[col].mean()
+        std = df[col].std()
+        new_data[col] = np.random.normal(loc=mean, scale=std, size=num_sample)
+    return pd.DataFrame(new_data)
+
+
+numerical_cols = ["open", "close", "adj_close", "low", "high", "volume"]
+new_numerical_data = generate_numerical_data(df, numerical_cols, num_samples)
+
+categorical_cols = ["ticker", "date", "exchange", "name", "sector", "industry"]
+new_categorical_data = (
+    df[categorical_cols].sample(n=num_samples, replace=True).reset_index(drop=True)
+)
+
+new_data = pd.concat([new_categorical_data, new_numerical_data], axis=1)
+new_data = new_data[df.columns]
+
+new_data = new_data.fillna("N/A")
+
+new_data.to_csv("data/1000.csv", index=False)
+
+
+result = pd.concat([df, new_data], axis=0, ignore_index=True)
+
+result.to_csv("data/merged_1000.csv", index=False)
